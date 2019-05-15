a85b9a6ad4d3b8e52b8a9e7f07696914825ddbfa
Aisho Alex <shinja@vivaldi.net>
2019-05-15 16:43:39 +0300

    Удалены неиспользуемые части кода

--- src/main/java/setTest.java
+++ src/main/java/setTest.java
@@ -1,44 +1,28 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.beans.Visibility;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.util.List;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfNestedElementLocatedBy;
 
 public class setTest {
-    private static boolean inArray(String value,String[] arr) {
-        for (String i:
-             arr) {
-            if(i.equalsIgnoreCase(value)) return true;
-        }
-        return false;
-    }
-
-    private static String checkCondition(boolean condition) {
-        if(condition) return "OK";
-        return "Please fix it";
-    }
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver.exe");
+//        System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = (WebDriverWait) new WebDriverWait(driver, 10, 1000).ignoring(NoSuchElementException.class);
         try {
             driver.get("http://aplana.ru");
-//            driver.navigate().refresh();
-//            driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+
             WebElement firstResult = ((ChromeDriver) driver).findElementByClassName("header-nav"); //wait.until(presenceOfElementLocated(By.cssSelector("header-menu > a")));
-//            for(int i = 0; i < firstResult.getSize(); )
+
             List<WebElement> elements = firstResult.findElements(By.xpath("./li/a"));
             String testHeaderMenuStrings[] = new String[] {"Компания", "Услуги", "Наш опыт",
                     "Инфо-центр", "Карьера", "Контакты"};
